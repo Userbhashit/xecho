6046fd2ae7e24b2973b929c2d6bb090b7af4fc73
Bhashit Gautam <bhashitgtm004@gmail.com>
2025-11-17 16:59:39 +0530

    Removes unnecessary comments.

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@ int main(const int argc, const char* argv[])
     // Parse command-line arguments to determine which flags are set
     const argv_flag flags = get_flag(argc, argv);
 
-    // Print the arguments according to the flags (handles -n, -e, etc.)
+    // // Print the arguments according to the flags (handles -n, -e, etc.)
     echo(argv, flags);
 
-    // Reset to default color
+    // // Reset to default color
     reset_color();
 
     return 0;
